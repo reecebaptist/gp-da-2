2a4e1644a19090cbcb123d2314d47aad43fa7c77
Reece D'Souza <rebads25@gmail.com>
2021-05-10 19:14:02 +0530

    New Commit

---
+++ Flappy_Bird/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreController : MonoBehaviour
+{
+    public static int score = 0;
+    public TextMeshProUGUI scoreText;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = score.ToString();
+    }
+}
